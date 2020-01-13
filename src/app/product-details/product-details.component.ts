fa29122fd531b8d19cb80a1370ab222cfb2f0964
s0440561 <sureeporn.eb@gmail.com>
2020-01-13 15:01:52 +0700

    create cart

---
+++ src/app/product-details/product-details.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { products } from '../products';//เพื่อดึงค่าproductมาแสดง
+import { CartService } from '../cart.service';
+
+@Component({
+  selector: 'app-product-details',
+  templateUrl: './product-details.component.html',
+  styleUrls: ['./product-details.component.css']
+})
+export class ProductDetailsComponent implements OnInit {
+
+  product;//เพื่อนำไปใช้ต่อในหน้าhtml
+  constructor(
+    private route : ActivatedRoute,
+    private cartService : CartService
+    )  { }
+
+  ngOnInit() {
+    this.route.paramMap.subscribe(params=>{
+      this.product = products[+params.get('productId')];
+    });
+  }
+
+  addToCart(product){
+    window.alert("Your product has been add to the cart!");
+    this.cartService.addToCart(product);
+    alert(this.cartService.items.length);
+  }
+
+}
